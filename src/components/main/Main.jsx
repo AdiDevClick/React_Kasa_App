37b38ee3fd698e91e77527dbde49938da58ae870
Adrien <adrienquijo@gmail.com>
2025-02-10 10:26:07 +0100

    Utilisation du RenderHeader générique Ajout de propTypes. Ajout JSDoc. Code cleanup

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,18 +1,29 @@
+import PropTypes from "prop-types";
 import { RenderCard } from "../cards/Card.jsx";
 import { RenderHeader } from "../headers/page-header/Header.jsx";
 import "assets/css/Card.scss";
+import headerImg from "assets/images/rivage-S.png";
 
 /**
- *
- * @param {object} param0 - L'objet contenant la SQL Query
+ * @param {object} param0
+ * @param {{}} param0.datas - L'objet contenant la SQL Query
  * @returns
  */
 export function RenderMain({ datas }) {
-    console.log(datas);
+    console.log("Render Main Page ");
     return (
         <main>
-            <RenderHeader />
+            <RenderHeader img={headerImg} alt={"Un rivage"} />
             <RenderCard className="card-container" datas={datas} />
         </main>
     );
 }
+
+RenderMain.propTypes = {
+    datas: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
