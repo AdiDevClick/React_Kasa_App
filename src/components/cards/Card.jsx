1c694a34ea3042199d2752cca4374be579b734c0
Adrien <adrienquijo@gmail.com>
2025-02-10 10:16:54 +0100

    Les cards génèreront un react router Link. Ajout des propTypes. Code Cleanup -

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,6 +1,9 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router";
+
 /**
- * Crer un élément ".card"
- * @param {string} className -
+ * Crer un élément ".card" à partir d'un objet
+ * @param {string} className - Le nom de la class CSS
  * @param {object} datas
  * @returns {JSX.Element}
  */
@@ -8,13 +11,30 @@ export function RenderCard({ className, datas }) {
     return (
         <section className={className}>
             {datas.map((element) => (
-                <a href={"#"} key={element.id}>
+                <Link to={"/" + element.id} key={element.id}>
                     <figure className="card">
                         <img src={element.cover} alt={element.title} />
                         <figcaption>{element.title}</figcaption>
                     </figure>
-                </a>
+                </Link>
+                // <a href={"#"} key={element.id}>
+                //     <figure className="card">
+                //         <img src={element.cover} alt={element.title} />
+                //         <figcaption>{element.title}</figcaption>
+                //     </figure>
+                // </a>
             ))}
         </section>
     );
 }
+
+RenderCard.propTypes = {
+    className: PropTypes.string.isRequired,
+    datas: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            cover: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
