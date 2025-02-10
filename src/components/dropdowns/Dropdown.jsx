449dda8fe9e6484797d28637eb0f442fabd7aceb
Adrien <adrienquijo@gmail.com>
2025-02-10 10:03:15 +0100

    Ajout d'un composant Dropdown qui prendra en paramètre un titre et son/ses textes.

---
+++ src/components/dropdowns/Dropdown.jsx
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import { useId, useState } from "react";
+import "assets/css/DropDown.scss";
+import arrow from "assets/images/dropdown-arrow.svg";
+
+/**
+ * Render un dropdown avec un bouton sur la droite
+ * @param {string} title - Titre du dropdown
+ * @param {string|Array} text - Texte du dropdown
+ * @returns {JSX.Element}
+ */
+export function RenderDropDown({ title, text }) {
+    const [clicked, isClicked] = useState(false);
+
+    const handleClick = (e, bool) => {
+        // Plays button's animation
+        e.currentTarget.classList.toggle("closed");
+        isClicked(bool);
+    };
+
+    text = Array.isArray(text) ? text : [text];
+
+    const id = useId();
+
+    return (
+        <div className="dropdown">
+            <div
+                className="dropdown_container open closed"
+                onClick={(e) => handleClick(e, !clicked)}
+            >
+                <h2 className="dropdown__title">{title}</h2>
+                <button aria-expanded={clicked} aria-controls={id}>
+                    <img className="dropdown__btn" src={arrow} />
+                </button>
+            </div>
+            {clicked && (
+                <span id={id}>
+                    {text.map((text, index) => (
+                        <p
+                            key={id + index}
+                            id={id + index}
+                            className="dropdown__text"
+                        >
+                            {text}
+                        </p>
+                    ))}
+                </span>
+            )}
+        </div>
+    );
+}
+
+RenderDropDown.propTypes = {
+    title: PropTypes.string.isRequired,
+    text: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.string),
+    ]).isRequired,
+};
