a0c47f78babd390f0369d3e087fe94659ee8e7d0
Adrien <adrienquijo@gmail.com>
2025-02-10 10:22:58 +0100

    Modification du main App(). Il prendra maintenant un children pour lui permettre d'être générique. Ajout de la JSDoc. Ajout des propTypes -

--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -3,20 +3,25 @@
 import "assets/css/reset.css";
 import "assets/css/App.css";
 import "assets/css/main.scss";
+import PropTypes from "prop-types";
 
-import { RenderMain } from "../../components/main/Main.jsx";
-import { RenderFooter } from "../../components/footer/Footer.jsx";
-import datas from "../../data/datas.json";
-function App() {
+/**
+ * Render le contenu principal de l'App
+ * Il prend en paramètre un Children JSX
+ * @param {Object} param0
+ * @param {JSX.Element} param0.children
+ * @returns {JSX.Element}
+ */
+function App({ children }) {
     return (
         <>
-            <div className="main-container">
-                {/* <RenderNavHeader /> */}
-                <RenderMain datas={datas} />
-            </div>
-            {/* <RenderFooter /> */}
+            <div className="main-container">{children}</div>
         </>
     );
 }
+
+App.propTypes = {
+    children: PropTypes.element.isRequired,
+};
 
 export default App;
