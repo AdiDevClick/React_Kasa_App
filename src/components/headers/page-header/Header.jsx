c05b1340afd43d5ed94b5907db32254a67561141
Adrien <adrienquijo@gmail.com>
2025-02-10 10:29:22 +0100

    Ajout JSDoc

--- src/components/headers/page-header/Header.jsx
+++ src/components/headers/page-header/Header.jsx
@@ -3,9 +3,10 @@ import PropTypes from "prop-types";
 
 /**
  * Render l'image Header des pages
- * @param {string} img - Le path de l'image
- * @param {string} alt - Le text du "alt" pour l'accessibilité
- * @param {Boolean} [text=true]  - Défini si oui / non le h1 sera display
+ * @param {object} param0
+ * @param {string} param0.img - Le path de l'image
+ * @param {string} param0.alt - Le text du "alt" pour l'accessibilité
+ * @param {Boolean} [param0.text=true]  - Défini si oui / non le h1 sera display
  * @returns
  */
 export function RenderHeader({ img, alt, text = true }) {
