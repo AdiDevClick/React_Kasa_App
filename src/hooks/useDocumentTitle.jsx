104fc56003e3883fd7a63b1021efb1db6e5bbc30
Adrien <adrienquijo@gmail.com>
2025-02-14 15:48:36 +0100

    création d'un hook qui permet d'ajouter le document title

---
+++ src/hooks/useDocumentTitle.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+/**
+ * Défini le titre du document et
+ * réassigne l'ancien lorsque l'on quitte la page -
+ * @param {string} docTitle - Le nom du document actuel
+ */
+export function useDocumentTitle(docTitle) {
+    useEffect(() => {
+        const oldTitle = document.title;
+        document.title = "Kasa - " + docTitle;
+
+        return () => {
+            document.title = oldTitle;
+        };
+    }, [docTitle]);
+}
