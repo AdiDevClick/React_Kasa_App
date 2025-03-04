b90836bb885b9ce156969b38ef80358348ab4d35
Adrien <adrienquijo@gmail.com>
2025-03-04 15:28:56 +0100

    Modification mineure - La div sera maintenant wrap dans un main

--- src/pages/Error/PageError.jsx
+++ src/pages/Error/PageError.jsx
@@ -1,6 +1,6 @@
-import { useRouteError } from "react-router";
-import { Error404 } from "./404/Error404.jsx";
-import "assets/css/404.scss";
+import { useRouteError } from 'react-router';
+import { Error404 } from './404/Error404.jsx';
+import 'assets/css/404.scss';
 
 /**
  * Display une erreur 404 si la route est mauvaise.
@@ -12,12 +12,14 @@ export function PageError() {
     if (error.status === 404) return <Error404 />;
 
     return (
-        <div style={{ color: "red" }} id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <main>
+            <div style={{ color: 'red' }} id="error-page">
+                <h1>Oops!</h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
+            </div>
+        </main>
     );
 }
