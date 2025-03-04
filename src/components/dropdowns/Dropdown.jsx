d4a6d5e1cd18879ae586945b86c764be032b4cfa
Adrien <adrienquijo@gmail.com>
2025-03-04 15:21:18 +0100

    Refactorisation du dropdown pour une meilleure visibilité - Ajout d'un listener transitionend pour linéariser l 'animation du span -

--- src/components/dropdowns/Dropdown.jsx
+++ src/components/dropdowns/Dropdown.jsx
@@ -1,65 +1,73 @@
-import PropTypes from "prop-types";
-import { useId, useState } from "react";
-import "assets/css/DropDown.scss";
-import arrow from "assets/images/dropdown-arrow.svg";
+import PropTypes from 'prop-types';
+import { useId, useRef, useState } from 'react';
+import 'assets/css/DropDown.scss';
+import arrow from 'assets/images/dropdown-arrow.svg';
+import { DropdownParagraph } from './DropdownParagraph.jsx';
+import { DropdownButton } from './DropdownButton.jsx';
 
 /**
- * Render un dropdown avec un bouton sur la droite
+ * Affiche un dropdown avec un bouton sur la droite -
+ * Une transition s'affichera quand l'utilisateur active le drawer -
  * @param {object} param0
- * @param {string} param0.title - Titre du dropdown
- * @param {string|Array} param0.text - Texte du dropdown
+ * @param {object} param0.item - Object contenant toutes les propriétés
  * @returns {JSX.Element}
  */
+export function DropDown({ item }) {
+    item.text = Array.isArray(item.text) ? item.text : [item.text];
 
-export function DropDown({ title, text }) {
-    const [clicked, isClicked] = useState(false);
-
-    const handleClick = (e, bool) => {
-        // Plays button's animation
-        e.currentTarget.classList.toggle("closed");
-        isClicked(bool);
-    };
-
-    text = Array.isArray(text) ? text : [text];
-
+    const [active, setActive] = useState(false);
     const id = useId();
+    const paragraphsRef = useRef();
+
+    /** @type {PointerEvent} */
+    const handleClick = (e) => {
+        setActive((click) => !click);
+
+        // Plays button's animation
+        e.currentTarget.classList.toggle('closed');
+
+        // Opacity fadein for the paragraphs
+        if (active) {
+            paragraphsRef.current.addEventListener(
+                'transitionend',
+                () => {
+                    // Display none to reset animation state
+                    paragraphsRef.current.style.display = 'none';
+                    paragraphsRef.current.removeAttribute('style');
+                },
+                { once: true }
+            );
+        }
+    };
 
     return (
         <div className="dropdown">
             <div
                 className="dropdown_container open closed"
-                onClick={(e) => handleClick(e, !clicked)}
+                onClick={handleClick}
             >
-                <h2 className="dropdown__title">{title}</h2>
-                <button aria-expanded={clicked} aria-controls={id}>
-                    <img
-                        alt="icône flêche du bouton"
-                        className="dropdown__btn"
-                        src={arrow}
-                    />
-                </button>
+                <h2 className="dropdown__title">{item.title}</h2>
+                <DropdownButton ariaId={id} isActive={active} arrow={arrow} />
             </div>
-            {clicked && (
-                <span id={id}>
-                    {text.map((text, index) => (
-                        <p
-                            key={id + index}
-                            id={id + index}
-                            className="dropdown__text"
-                        >
-                            {text}
-                        </p>
-                    ))}
-                </span>
-            )}
+
+            <span
+                ref={paragraphsRef}
+                id={id}
+                className={active ? 'open' : 'closed'}
+            >
+                {item.text.map((text, index) => (
+                    <DropdownParagraph
+                        key={item.id + index}
+                        id={item.id + index}
+                    >
+                        {text}
+                    </DropdownParagraph>
+                ))}
+            </span>
         </div>
     );
 }
 
 DropDown.propTypes = {
-    title: PropTypes.string.isRequired,
-    text: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.arrayOf(PropTypes.string),
-    ]).isRequired,
+    item: PropTypes.object.isRequired,
 };
