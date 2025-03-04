e0ff74391255f788d7d3c873af35f1e963eee2ef
Adrien <adrienquijo@gmail.com>
2025-03-04 16:19:28 +0100

    Ajout d'un composant Tag - Il prend en children le texte a insérer

--- src/components/tags/Tags.jsx
+++ src/components/tags/Tags.jsx
@@ -1,24 +1,15 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 /**
- * Crer des tags en fonction de l'array passé en props
- * @param {object} param0 - props
- * @param {array} param0.datas - Array de tags à construire.
- * @returns {JSX.Element}
+ * Affiche un Tag contenant un texte
+ * @param {object} param0
+ * @param {string} param0.children - Texte du Tag
+ * @returns
  */
-export function Tags({ datas }) {
-    Array.isArray(datas) ? datas : [datas];
-    return (
-        <ul className="tags_container">
-            {datas.map((tag, index) => (
-                <li className="tag" key={index}>
-                    {tag}
-                </li>
-            ))}
-        </ul>
-    );
+export function Tags({ children }) {
+    return <li className="tag">{children}</li>;
 }
 
 Tags.propTypes = {
-    datas: PropTypes.oneOfType([PropTypes.array, PropTypes.string]).isRequired,
+    children: PropTypes.string.isRequired,
 };
