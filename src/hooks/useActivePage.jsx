a525a57711fde2bb554e855a9e8db2a25d64204c
Adrien <adrienquijo@gmail.com>
2025-04-11 16:07:30 +0200

    Ajout JS Doc

--- src/hooks/useActivePage.jsx
+++ src/hooks/useActivePage.jsx
@@ -1,4 +1,11 @@
-import { useState } from "react";
+/**
+ * @description Déprécié - Utilisé pour gérer la page active dans le menu de navigation
+ * Créé par Adrien Quijo
+ *
+ * V 1.0.0
+ */
+
+import { useState } from 'react';
 
 export function useActivePage() {
     const [activePage, setActivePage] = useState(null);
